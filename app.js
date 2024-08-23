f9fcbd8b0f4d1fc968e0a859c978471d2549094f
austin-area61 <austinonyango.area61@gmail.com>
2024-08-23 07:39:31 +0300

    wrapping jsx elements in a single outer 'div' element.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const myArticle = <article></article>;
 const p1 = <p id ='large'>foo</p>;
 const p2 = <p id ='small'>bar</p>;
+
+const newParagraph = (
+  <div>
+    <h1>Austin is learning React.js😊</h1>
+    <p>Its really interesting!</p>
+  </div>
+);
+const blog = (
+  <div id = 'austin-blog'>
+    <img src="pics/192940u73.jpg" />
+    <h1>Welcome to Austin's Blog!</h1>
+    <article>
+      Wow I had the tastiest sandwich today. I <strong>literally</strong> almost
+      freaked out.
+    </article>
+  </div>
+);
